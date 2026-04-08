86f6aca6bd9c927d20646bb7953a362386f49b26
Juan Garcia <juan3722880@gmail.com>
2026-04-08 16:58:30 -0500

    Added javaScript code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 function init(){
 //add your javascrip between these two lines of code
-    
+  var button = document.getElementById("entrybutton");
+  var usrInput = document.getelementById("entryinput");
+  var output = document.getElementById("textoutput");
 
-
+  button.addEventListener("click", function() {
+    var usrText= usrInput.value;
+    alert("Juan Garcia: " + usrText);
+    output.textContent = usrText;
+  });
 
 
 
