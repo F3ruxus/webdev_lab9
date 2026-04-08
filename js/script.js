2ff7bc079cc82297ed682159a781a631025598d1
Juan Garcia <juan3722880@gmail.com>
2026-04-08 17:37:27 -0500

    Cleaning up code and formatting

--- js/script.js
+++ js/script.js
@@ -5,13 +5,9 @@ function init(){
   function handleClick() {
     var usrInput = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = usrInput.value;
-    
     alert("Juan Garcia: " + usrInput.value);
   }
-  button.addEventListener("click", handleClick);
-
-
-
-
+  
+  button.addEventListener('click', handleClick);
 }
 window.addEventListener('load', init);
